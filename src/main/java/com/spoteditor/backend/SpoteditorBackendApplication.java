5cf856bbe65b7ab81d7d2f2e0d9bbfb1f7a82916
Sangahn <sangahn0524@gmail.com>
2025-01-17 11:49:53 +0900

    [init] Spoteditor Backend 최초 커밋

---
+++ src/main/java/com/spoteditor/backend/SpoteditorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spoteditor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoteditorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoteditorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spoteditor/backend/SpoteditorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoteditor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoteditorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
