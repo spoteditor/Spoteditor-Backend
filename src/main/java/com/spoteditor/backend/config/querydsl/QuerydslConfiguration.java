ed4168ba8808ad1c5ca4bf0e0a9883f3b9c4b9ed
jwj <dnwls16071@naver.com>
2025-01-24 00:44:36 +0900

    [config🔧] Querydsl 관련 설정 추가(#2)

---
+++ src/main/java/com/spoteditor/backend/config/querydsl/QuerydslConfiguration.java
@@ -0,0 +1,22 @@
+package com.spoteditor.backend.config.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfiguration {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+
+}
