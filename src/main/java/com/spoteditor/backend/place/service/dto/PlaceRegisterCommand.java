b02ff28d240e987d6e5f6544b72484e35e750688
jwj <dnwls16071@naver.com>
2025-01-31 16:51:17 +0900

    [feat✨] 장소 비즈니스 레이어 코드 및 서비스 DTO 추가(#9)

---
+++ src/main/java/com/spoteditor/backend/place/service/dto/PlaceRegisterCommand.java
@@ -0,0 +1,27 @@
+package com.spoteditor.backend.place.service.dto;
+
+import com.spoteditor.backend.place.entity.Address;
+import com.spoteditor.backend.place.entity.Category;
+import com.spoteditor.backend.place.entity.Place;
+import com.spoteditor.backend.user.entity.User;
+
+public record PlaceRegisterCommand(
+
+		String name,
+		String description,
+		String originalFile,
+		Address address,
+		Category category
+) {
+
+	public Place toEntity(User user) {
+		return Place.builder()
+				.user(user)
+				.address(address)
+				.description(description)
+ 				.category(category)
+				.name(name)
+				.build();
+	}
+
+}
