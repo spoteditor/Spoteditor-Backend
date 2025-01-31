9a66b6de61a2a2fcf46de8e56709e3af9dad9818
jwj <dnwls16071@naver.com>
2025-01-31 17:01:15 +0900

    [feat✨️] JPA 자동 감지 설정 코드 추가(#9)

---
+++ src/main/java/com/spoteditor/backend/config/jpa/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.spoteditor.backend.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
