41e48ea16f44896ed8ffc10b34ed246d53267e6a
jwj <dnwls16071@naver.com>
2025-01-31 17:00:58 +0900

    [feat✨️] 커스텀 Page 코드 추가(#9)

---
+++ src/main/java/com/spoteditor/backend/config/page/PageRequest.java
@@ -0,0 +1,32 @@
+package com.spoteditor.backend.config.page;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import static org.springframework.data.domain.Sort.Direction;
+
+@Getter @Setter
+public class PageRequest {
+
+	private int page;
+	private int size;
+	private Direction direction;
+
+	public void setPage(int page) {
+		this.page = page <= 0 ? 1 : page;
+	}
+
+	public void setSize(int size) {
+		int DEFAULT_SIZE = 20;
+		int MAX_SIZE = 40;
+		this.size = size > MAX_SIZE ? DEFAULT_SIZE : size;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+
+	public org.springframework.data.domain.PageRequest of() {
+		return org.springframework.data.domain.PageRequest.of(page - 1, size, direction, "created_at");
+	}
+}
