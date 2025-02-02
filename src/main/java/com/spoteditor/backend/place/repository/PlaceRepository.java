e286cf5558c6d8bd348868060bc925157f0fb2a4
jwj <dnwls16071@naver.com>
2025-02-02 11:44:18 +0900

    [fix🐛] 낙관적 락 코드 부분 수정(#9)

--- src/main/java/com/spoteditor/backend/place/repository/PlaceRepository.java
+++ src/main/java/com/spoteditor/backend/place/repository/PlaceRepository.java
@@ -1,16 +1,8 @@
 package com.spoteditor.backend.place.repository;
 
 import com.spoteditor.backend.place.entity.Place;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
 
-	@Lock(LockModeType.OPTIMISTIC)
-	@Query("SELECT p FROM Place p WHERE p.id = :id")
-	Optional<Place> findByIdWithOptimisticLock(Long id);
 }
