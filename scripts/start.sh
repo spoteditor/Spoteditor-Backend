a2a79d48b3a2224c0bb7c7b83e0e3debde05b7b8
jwj <dnwls16071@naver.com>
2025-02-04 02:30:33 +0900

    [chore⚙️] Docker Container가 뜨지 않는 문제 수정(#11)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
+echo "--------------- 서버 배포 시작 -----------------"
 docker compose down || true
 docker pull 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com/spoteditor:latest
 docker compose up -d --build
+echo "--------------- 서버 배포 끝 -----------------"
