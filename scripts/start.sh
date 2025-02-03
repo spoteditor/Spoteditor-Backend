645f6d04a1308dc0b191f86173412ff95b72bc04
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2025-02-04 02:13:19 +0900

    [chore⚙️] 쉘 스크립트 수정(#11)
    - ECR 리포지토리 추가
    - Private Repository이고 또한 IAM 권한 제한
    - Access Key 관리를 잘한다면 ECR에 접근할 방법이 없어 직접 노출해도 무방할 것 같다고 판단 → 추후 바꿔야 할 것 같다고 판단이 된다면 보안 파일을 따로 작성해 EC2 서버에 전달하는 방법을 고려

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 docker compose down || true
-docker pull ${{ steps.login-ecr.outputs.registry }}/spoteditor:latest
+docker pull 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com/spoteditor/latest
 docker compose up -d --build
