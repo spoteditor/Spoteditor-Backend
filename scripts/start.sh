e4453a1d23722c23e3b219d74afd513b21f93de8
jwj <dnwls16071@naver.com>
2025-02-04 23:35:15 +0900

    [chore⚙️] 오타 수정(#11)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/SpotEditor-Backend
+cd /home/ubuntu/Spoteditor-Backend
 docker compose down || true
 docker pull 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com/spoteditor:latest
 docker compose up -d --build
