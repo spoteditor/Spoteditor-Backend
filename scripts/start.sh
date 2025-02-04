7ca976046c00291eb214f00ae96b66f10d4f8aed
jwj <dnwls16071@naver.com>
2025-02-04 21:01:28 +0900

    [ci💚] 쉘 스크립트 경로 이동 적용(#11)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 echo "--------------- 서버 배포 시작 -----------------"
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com
-
+cd ..
 docker compose down || true
 docker pull 891612578477.dkr.ecr.ap-northeast-2.amazonaws.com/spoteditor:latest
 docker compose up -d --build
