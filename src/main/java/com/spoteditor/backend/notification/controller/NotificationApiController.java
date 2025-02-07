c1c79c54fa0198aa5ff6b70aca25978e1750ed4e
jwj <dnwls16071@naver.com>
2025-02-08 00:17:44 +0900

    [feat✨️] 알림 프레젠테이션 레이어 코드 및 컨트롤러 DTO 추가(#14)

---
+++ src/main/java/com/spoteditor/backend/notification/controller/NotificationApiController.java
@@ -0,0 +1,52 @@
+package com.spoteditor.backend.notification.controller;
+
+import com.spoteditor.backend.config.page.CustomPageRequest;
+import com.spoteditor.backend.config.page.CustomPageResponse;
+import com.spoteditor.backend.notification.controller.dto.NotificationListDto;
+import com.spoteditor.backend.notification.dto.NotificationDto;
+import com.spoteditor.backend.notification.repository.NotificationRepository;
+import com.spoteditor.backend.notification.service.NotificationService;
+import com.spoteditor.backend.user.common.dto.UserIdDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NotificationApiController {
+
+	private final NotificationRepository notificationRepository;
+	private final NotificationService notificationService;
+
+	@PostMapping("/notice")
+	public ResponseEntity<Void> notice(@AuthenticationPrincipal UserIdDto dto, @RequestBody NotificationDto notificationDto) {
+
+		notificationService.send(notificationDto);
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.build();
+	}
+
+	@GetMapping("/notice")
+	public ResponseEntity<CustomPageResponse<NotificationListDto>> noticeList(@AuthenticationPrincipal UserIdDto dto, CustomPageRequest request) {
+
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(notificationRepository.notificationList(dto.getId(), request));
+	}
+
+	@PutMapping("/notice/{id}")
+	public ResponseEntity<Void> read(@AuthenticationPrincipal UserIdDto dto) {
+		return null;
+	}
+
+	@PutMapping("/notice")
+	public ResponseEntity<Void> readAll(@AuthenticationPrincipal UserIdDto dto) {
+		return null;
+	}
+}
