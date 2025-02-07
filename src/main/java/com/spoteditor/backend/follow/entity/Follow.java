f338f8d59bc76588d7b92137366df8774bd6903a
jwj <dnwls16071@naver.com>
2025-02-08 00:11:49 +0900

    [feat✨️] 팔로우 엔티티 코드 추가(#14)

---
+++ src/main/java/com/spoteditor/backend/follow/entity/Follow.java
@@ -0,0 +1,34 @@
+package com.spoteditor.backend.follow.entity;
+
+import com.spoteditor.backend.global.common.BaseEntity;
+import com.spoteditor.backend.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "follow_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "follower_id")
+	private User follower;	// 팔로워
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "following_id")
+	private User following; // 팔로잉
+
+	@Builder
+	private Follow(User following, User follower) {
+		this.following = following;
+		this.follower = follower;
+	}
+}
