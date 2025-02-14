3d6c66f04818ac8d06bd8015b187eeb6f16cecee
Sangahn <sangahn0524@gmail.com>
2025-02-14 15:58:36 +0900

    [feat✨] TagResponse 추가 (#20)

--- src/main/java/com/spoteditor/backend/tag/controller/dto/TagResponse.java
+++ src/main/java/com/spoteditor/backend/tag/controller/dto/TagResponse.java
@@ -1,4 +1,13 @@
 package com.spoteditor.backend.tag.controller.dto;
 
-public class TagResponse {
+import com.spoteditor.backend.tag.entity.Tag;
+import com.spoteditor.backend.tag.entity.TagCategory;
+
+public record TagResponse (
+        String name,
+        TagCategory category
+) {
+    public static TagResponse from(Tag tag) {
+        return new TagResponse(tag.getName(), tag.getTagCategory());
+    }
 }
