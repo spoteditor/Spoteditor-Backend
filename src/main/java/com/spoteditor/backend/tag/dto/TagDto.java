5371a3f85310fa61839115f4dafd2fae9973c8cb
Sangahn <sangahn0524@gmail.com>
2025-02-21 15:07:19 +0900

    [refactor♻️] TagDto 통일 (#58)

--- src/main/java/com/spoteditor/backend/tag/dto/TagDto.java
+++ src/main/java/com/spoteditor/backend/tag/dto/TagDto.java
@@ -1,4 +1,16 @@
 package com.spoteditor.backend.tag.dto;
 
-public class TagDto {
+import com.spoteditor.backend.tag.entity.Tag;
+import com.spoteditor.backend.tag.entity.TagCategory;
+
+public record TagDto (
+        String name,
+        TagCategory category
+) {
+    public static TagDto from(Tag tag) {
+        return new TagDto(
+                tag.getName(),
+                tag.getTagCategory()
+        );
+    }
 }
