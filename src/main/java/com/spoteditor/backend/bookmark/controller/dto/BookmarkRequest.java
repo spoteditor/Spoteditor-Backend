63234282ae21b70111203bf7c21c8eb4ebd169b5
jwj <dnwls16071@naver.com>
2025-02-25 16:18:42 +0900

    [fix🐛] @Builder 어노테이션 추가 및 DTO 간 네이밍 수정(#38)
    - 컨트롤러 DTO를 서비스 DTO로 변경하려는 의도를 명확히 하기 위해 toCommandDto로 메서드 네임을 변경합니다.

--- src/main/java/com/spoteditor/backend/bookmark/controller/dto/BookmarkRequest.java
+++ src/main/java/com/spoteditor/backend/bookmark/controller/dto/BookmarkRequest.java
@@ -1,13 +1,15 @@
 package com.spoteditor.backend.bookmark.controller.dto;
 
 import com.spoteditor.backend.bookmark.service.dto.BookmarkCommand;
+import lombok.Builder;
 
+@Builder
 public record BookmarkRequest(
 
 		Long placeId
 ) {
 
-	public BookmarkCommand from() {
+	public BookmarkCommand toCommandDto() {
 		return new BookmarkCommand(
 				this.placeId
 		);
