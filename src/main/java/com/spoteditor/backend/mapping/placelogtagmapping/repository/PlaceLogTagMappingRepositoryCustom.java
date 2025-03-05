39e7a72b509cfedf7ca91c537991e1ddc8be2f36
Sangahn <sangahn0524@gmail.com>
2025-03-05 10:46:09 +0900

    [feat✨] 로그 수정기능 PlaceLogTagMapping Repository 기능 추가 (#90)

--- src/main/java/com/spoteditor/backend/mapping/placelogtagmapping/repository/PlaceLogTagMappingRepositoryCustom.java
+++ src/main/java/com/spoteditor/backend/mapping/placelogtagmapping/repository/PlaceLogTagMappingRepositoryCustom.java
@@ -2,10 +2,13 @@ package com.spoteditor.backend.mapping.placelogtagmapping.repository;
 
 
 import com.spoteditor.backend.mapping.placelogtagmapping.entity.PlaceLogTagMapping;
+import com.spoteditor.backend.tag.entity.Tag;
 
 import java.util.List;
 
 public interface PlaceLogTagMappingRepositoryCustom {
 
     List<PlaceLogTagMapping> findByPlaceLogId(Long placeLogId);
+
+    List<PlaceLogTagMapping> findByPlaceLogAndTagIn(Long placeLogId, List<Long> tagIds);
 }
