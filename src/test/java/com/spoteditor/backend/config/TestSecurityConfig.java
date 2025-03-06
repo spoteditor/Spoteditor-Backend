033631c7407b7a99b9340553b39058d49047efb4
Sangahn <sangahn0524@gmail.com>
2025-03-06 13:09:21 +0900

    [test✅] Test용 SecurityFilter, JwtFilter 추가 (#99)

---
+++ src/test/java/com/spoteditor/backend/config/TestSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spoteditor.backend.config;
+
+import com.spoteditor.backend.config.jwt.JwtFilter;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+@TestConfiguration
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .addFilterBefore(new TestJwtFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
