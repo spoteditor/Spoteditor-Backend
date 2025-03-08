34bfd71937c2b8c9ebcf9b7b1d2a679a0d4ea028
Sangahn <sangahn0524@gmail.com>
2025-03-09 02:12:36 +0900

    [feat✨] PlaceLog 북마크 확인기능 추가 (#111)

--- src/main/java/com/spoteditor/backend/mapping/userplacelogmapping/repository/UserPlaceLogMappingRepositoryImpl.java
+++ src/main/java/com/spoteditor/backend/mapping/userplacelogmapping/repository/UserPlaceLogMappingRepositoryImpl.java
@@ -1,7 +1,26 @@
 package com.spoteditor.backend.mapping.userplacelogmapping.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import static com.spoteditor.backend.mapping.userplacelogmapping.entity.QUserPlaceLogMapping.userPlaceLogMapping;
+
 @Repository
+@RequiredArgsConstructor
 public class UserPlaceLogMappingRepositoryImpl implements  UserPlaceLogMappingRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public boolean existsByUserIdAndPlaceLogId(Long userId, Long placeLogId) {
+        Integer result = queryFactory
+                .selectOne()
+                .from(userPlaceLogMapping)
+                .where(userPlaceLogMapping.user.id.eq(userId))
+                .where(userPlaceLogMapping.placeLog.id.eq(placeLogId))
+                .fetchFirst();
+
+        return result != null;
+    }
 }
