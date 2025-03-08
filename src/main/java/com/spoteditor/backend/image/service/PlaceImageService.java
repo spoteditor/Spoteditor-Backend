58f9abb2ba2f0313d47d293f53724eb005cfc3cd
Sangahn <sangahn0524@gmail.com>
2025-03-09 00:08:12 +0900

    [feat✨] PlaceImage에 메인버킷->템프버킷 이동, 메인버킷 이미지 삭제기능 추가 (#90)

--- src/main/java/com/spoteditor/backend/image/service/PlaceImageService.java
+++ src/main/java/com/spoteditor/backend/image/service/PlaceImageService.java
@@ -3,11 +3,13 @@ package com.spoteditor.backend.image.service;
 import com.spoteditor.backend.image.controller.dto.PlaceImageResponse;
 import com.spoteditor.backend.image.controller.dto.PreSignedUrlResponse;
 import com.spoteditor.backend.image.controller.dto.PreSignedUrlRequest;
-import com.spoteditor.backend.image.entity.PlaceImage;
+import com.spoteditor.backend.placelog.event.dto.PlaceLogPlaceImage;
 
 public interface PlaceImageService {
 
 	PreSignedUrlResponse processPreSignedUrl(PreSignedUrlRequest request);
 	PlaceImageResponse upload(String originalFile, String uuid, Long placeId);
 	PlaceImageResponse uploadWithoutPlace(String originalFile, String uuid);
+	void rollbackUpload(PlaceLogPlaceImage image);
+	void deleteMainBucketImage(PlaceLogPlaceImage image);
 }
