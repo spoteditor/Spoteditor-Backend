ecf0f99b182c5de0678468143887d4744b904f68
jwj <dnwls16071@naver.com>
2025-03-08 19:15:52 +0900

    [feat✨️] 공간별 북마크 여부 조회 API 추가(#110)

--- src/main/java/com/spoteditor/backend/placelog/controller/PlaceLogBookmarkController.java
+++ src/main/java/com/spoteditor/backend/placelog/controller/PlaceLogBookmarkController.java
@@ -1,7 +1,9 @@
 package com.spoteditor.backend.placelog.controller;
 
 import com.spoteditor.backend.config.swagger.PlaceLogBookmarkApiDocument;
+import com.spoteditor.backend.placelog.controller.dto.PlaceLogBookmarkResponse;
 import com.spoteditor.backend.placelog.service.PlaceLogBookmarkService;
+import com.spoteditor.backend.placelog.service.PlaceLogService;
 import com.spoteditor.backend.user.common.dto.UserIdDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,12 +11,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PlaceLogBookmarkController implements PlaceLogBookmarkApiDocument {
 
     private final PlaceLogBookmarkService placeLogBookmarkService;
+    private final PlaceLogService placeLogService;
 
     @Override
     @PostMapping("/placelogs/{placeLogId}/bookmark")
@@ -40,5 +45,16 @@ public class PlaceLogBookmarkController implements PlaceLogBookmarkApiDocument {
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
+    }
+
+    @GetMapping("/placelogs/{placeLogId}/bookmark")
+    public ResponseEntity<List<PlaceLogBookmarkResponse>> getBookmarkStatus(
+            @AuthenticationPrincipal UserIdDto userIdDto,
+            @PathVariable Long placeLogId
+    ) {
+        List<PlaceLogBookmarkResponse> placeBookmarkStatus = placeLogService.getPlaceBookmarkStatus(userIdDto.getId(), placeLogId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(placeBookmarkStatus);
     }
 }
