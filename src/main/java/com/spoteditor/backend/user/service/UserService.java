b82f5c6e72e1701845261cca92761241420cc2e6
Sangahn <sangahn0524@gmail.com>
2025-03-10 14:18:06 +0900

    [feat✨] UserService에 isFollowing 담는 로직 추가 (#125)

--- src/main/java/com/spoteditor/backend/user/service/UserService.java
+++ src/main/java/com/spoteditor/backend/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.spoteditor.backend.user.service;
 
 import com.spoteditor.backend.user.entity.User;
+import com.spoteditor.backend.user.service.dto.OtherUserResult;
 import com.spoteditor.backend.user.service.dto.UserResult;
 import com.spoteditor.backend.user.service.dto.UserUpdateCommand;
 import com.spoteditor.backend.user.service.dto.UserUpdateResult;
@@ -8,6 +9,8 @@ import com.spoteditor.backend.user.service.dto.UserUpdateResult;
 public interface UserService {
 
     UserResult getUser(Long userId);
+
+    OtherUserResult getOtherUser(Long userId, Long otherUserId);
 
     UserUpdateResult updateUser(Long userId, UserUpdateCommand command);
 
--- src/main/java/com/spoteditor/backend/user/service/UserServiceImpl.java
+++ src/main/java/com/spoteditor/backend/user/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.spoteditor.backend.placelog.entity.PlaceLog;
 import com.spoteditor.backend.placelog.repository.PlaceLogRepository;
 import com.spoteditor.backend.user.entity.User;
 import com.spoteditor.backend.user.repository.UserRepository;
+import com.spoteditor.backend.user.service.dto.OtherUserResult;
 import com.spoteditor.backend.user.service.dto.UserResult;
 import com.spoteditor.backend.user.service.dto.UserUpdateCommand;
 import com.spoteditor.backend.user.service.dto.UserUpdateResult;
@@ -17,8 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static com.spoteditor.backend.global.response.ErrorCode.DELETED_USER;
-import static com.spoteditor.backend.global.response.ErrorCode.NOT_FOUND_USER;
+import static com.spoteditor.backend.global.response.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -42,6 +42,34 @@ public class UserServiceImpl implements UserService{
         Long following = followRepository.countFollowing(userId);
 
         return new UserResult(user, follower, following);
+    }
+
+    @Override
+    @Transactional
+    public OtherUserResult getOtherUser(Long userId, Long otherUserId) {
+
+        UserResult userResult = getUser(otherUserId);
+        boolean isFollowing;
+
+        if(userId == null) {
+            isFollowing = false;
+        }
+        else {
+            if(userId.equals(otherUserId)) {
+                throw new UserException(NOT_OTHER_USER);
+            }
+
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UserException(NOT_FOUND_USER));
+
+            if(user.isDeleted()) {
+                throw new UserException(DELETED_USER);
+            }
+
+            isFollowing = followRepository.findIsFollowing(userId, otherUserId);
+        }
+
+        return OtherUserResult.from(userResult.user(), userResult.follower(), userResult.following(), isFollowing);
     }
 
     @Override
