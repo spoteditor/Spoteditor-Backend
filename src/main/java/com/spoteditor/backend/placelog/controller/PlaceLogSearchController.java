c09c7958b12155d97912c61f9d320667bdeb75c5
Sangahn <sangahn0524@gmail.com>
2025-03-12 11:50:38 +0900

    [feat✨] 검색 Controller 추가 (#129)

---
+++ src/main/java/com/spoteditor/backend/placelog/controller/PlaceLogSearchController.java
@@ -0,0 +1,47 @@
+package com.spoteditor.backend.placelog.controller;
+
+import com.spoteditor.backend.config.page.CustomPageRequest;
+import com.spoteditor.backend.config.page.CustomPageResponse;
+import com.spoteditor.backend.global.exception.PlaceLogException;
+import com.spoteditor.backend.placelog.controller.dto.PlaceLogListResponse;
+import com.spoteditor.backend.placelog.repository.PlaceLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.spoteditor.backend.global.response.ErrorCode.SEARCH_MINIMUM_REQUIRED_LETTERS;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PlaceLogSearchController {
+
+    private final PlaceLogRepository placeLogRepository;
+
+    @GetMapping("/search/placelogs/address")
+    public ResponseEntity<CustomPageResponse<PlaceLogListResponse>> getPlaceLogsByAddress(
+            @ModelAttribute CustomPageRequest pageRequest,
+            @RequestParam String sido,
+            @RequestParam String sigungu
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(placeLogRepository.searchBySidoSigungu(pageRequest, sido, sigungu));
+    }
+
+    @GetMapping("/search/placelogs/name")
+    public ResponseEntity<CustomPageResponse<PlaceLogListResponse>> getPlaceLogsByName(
+            @ModelAttribute CustomPageRequest pageRequest,
+            @RequestParam String name
+    ) {
+        String searchName = name.trim();
+        if(searchName.length() < 2) {
+            throw new PlaceLogException(SEARCH_MINIMUM_REQUIRED_LETTERS);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(placeLogRepository.searchByName(pageRequest, searchName));
+    }
+}
