07c81ba72068281fb33c54c00ea131c6cdd1ab9c
jwj <dnwls16071@naver.com>
2025-03-14 20:31:25 +0900

    [refactor♻️] [현준님 피드백] - 알림 메시지 포멧 형식 변경(#132)

--- src/main/java/com/spoteditor/backend/notification/dto/NotificationMessage.java
+++ src/main/java/com/spoteditor/backend/notification/dto/NotificationMessage.java
@@ -2,11 +2,9 @@ package com.spoteditor.backend.notification.dto;
 
 public class NotificationMessage {
 
-	private static final String PREFIX = "[#] 알림: ";
+	public static final String FOLLOW = "%s님이 팔로우했습니다.";
 
-	public static final String FOLLOW = PREFIX + "%s님이 %s님을 팔로우했습니다.";
-
-	public static String formatFollowMessage(String followerName, String followingName) {
-		return String.format(FOLLOW, followerName, followingName);
+	public static String formatFollowMessage(String followerName) {
+		return String.format(FOLLOW, followerName);
 	}
 }
