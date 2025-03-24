5ce6a40c45e6cb6d516ab890188ba93217e51bcc
Sangahn <sangahn0524@gmail.com>
2025-03-24 15:19:00 +0900

    [refactor♻️] 필요없는 로직 제거 (#147)

--- src/main/java/com/spoteditor/backend/placelog/service/dto/PlaceLogRegisterCommand.java
+++ src/main/java/com/spoteditor/backend/placelog/service/dto/PlaceLogRegisterCommand.java
@@ -34,40 +34,4 @@ public record PlaceLogRegisterCommand (
                 request.places()
         );
     }
-
-    public PlaceLog toEntity(User user, List<Place> places, List<Tag> tags, PlaceImage placeLogImage) {
-
-        Place firstPlace = places.get(0);
-
-        PlaceLog placeLog = PlaceLog.builder()
-                .user(user)
-                .name(name)
-                .description(description)
-                .placeLogImage(placeLogImage)
-                .address(firstPlace.getAddress())
-                .status(status)
-                .build();
-
-        // 로그 - 장소 중간테이블
-        List<PlaceLogPlaceMapping> placeLogPlaceMappings = places.stream()
-                .map(place -> PlaceLogPlaceMapping.builder()
-                        .placeLog(placeLog)
-                        .place(place)
-                        .build())
-                .toList();
-
-        placeLog.getPlaceLogPlaceMappings().addAll(placeLogPlaceMappings);
-
-        // 로그 - 태그 중간테이블
-        List<PlaceLogTagMapping> placeLogTagMappings = tags.stream()
-                .map(tag -> PlaceLogTagMapping.builder()
-                        .placeLog(placeLog)
-                        .tag(tag)
-                        .build())
-                .toList();
-
-        placeLog.getPlaceLogTagMappings().addAll(placeLogTagMappings);
-
-        return placeLog;
-    }
 }
